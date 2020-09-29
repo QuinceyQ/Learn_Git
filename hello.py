79ac6f646f08405e1779949e602e82e89ade5b7c
Quincy <1300456743@qq.com>
2020-09-29 20:57:27 +0800

    "Testing for git-push"

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print('hello world')
+class Dog:
+    kind = ''
+    def _init_(self, name):
+        self.name = name
+    def bark(self):
+        return 'woof-woof'
+
+a = Dog('Fido')
+b = Dog('Buddy')
+        
